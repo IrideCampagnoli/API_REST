7750be3e881e6ee972dd642a2c3eb0d71ab591cc
Iride U <60908594+IrideCampagnoli@users.noreply.github.com>
2022-07-21 20:23:21 -0300

    version_2

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+
+app.get('/', (req,res) => {
+  res.send('ola')
+})
+
 app.listen(4000, () => {
   console.log('API RODANDO!')
 } )
